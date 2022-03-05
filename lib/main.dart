0d4c6c9ef33131ce63bf1f9067c6a0e6c63627a0
Ufuk <ufukkt2699@hotmail.com>
2022-03-05 23:35:02 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,169 +1,174 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:widget/contants/app_text_contants.dart';
-import 'package:widget/widget/stateles_widget_page.dart';
-import 'package:kartal/kartal.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import 'widget/stateful_widget_page.dart';
-import 'widget/stateles_widget_page.dart';
-import 'contants/app_color_contants.dart';
+import 'pages/body_chats.dart';
+import 'pages/body_status.dart';
+import 'pages/body_call.dart';
+import 'pages/body_settings.dart';
+import 'ui/helper/text_helper.dart';
+import 'ui/helper/color_helper.dart';
+import 'ui/helper/icon_helper.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyHomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String denemeabc;
+    CollectionReference messageBoxRef =
+        FirebaseFirestore.instance.collection('messagebox');
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: StatelessPage(),/*Scaffold(
+      title: TextHelper.headerTitle,
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+          future: messageBoxRef.doc().get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+              // bool error = true;
+              //return HomePage(error);
+            } else if (snapshot.hasData) {
+              //bool error = false;
+              return HomePage();
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference messageBoxRef = _firestore.collection('');
+
+    DocumentReference messageRef = messageBoxRef.doc('');
+
+    void firebaseDbAddData() {
+      messageBoxRef
+          .add({
+            '_users': 'abc',
+            'archive': false,
+            'message': 'deneme mesaj',
+            'user': 'Ufuuk Küçüktopçu',
+          })
+          .then((value) => print('Kullanıcı başarıyla veritabanına eklendi'))
+          .catchError((error) => print(
+              'Kullanıcıyı veritabanına eklerken bir sorunla karşılaştık.'));
+    }
+
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
         appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              color: Colors.white,
-              child: Center(
-                  child: Text('U',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18))),
+          backgroundColor: ColorHelper.headerColor,
+          leading: Container(
+            width: 50.0,
+            height: 50.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: IconHelper.headerCameraIcon,
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                primary: Colors.transparent,
+              ),
             ),
+          ),
+          title: Text(
+            TextHelper.headerTitle,
+            style: TextStyle(color: ColorHelper.headerTextColor),
           ),
           actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.account_box_outlined),
-            ),
-          ],
-          centerTitle: true,
-          backgroundColor: Colors.black,
-          elevation: 4,
-          title: const Text('Uber'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  'Hoşgeldiniz.',
-                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    // Expanded tek bir satırda kendine kalan tüm alanı kaplar
-                    flex: 5,
-                    child: Container(
-                      color: Colors.blueGrey.withOpacity(0.5),
-                      height: 50,
-                      //width: 300,
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Text(
-                              'Ara',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
-                            ),
-                          )),
+            Row(
+              children: [
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: IconHelper.headerSearchIcon,
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: Colors.transparent,
                     ),
                   ),
-                  const Expanded(flex: 1, child: Icon(Icons.search)),
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Geçmiş Yolculuklarınız',
-                style: TextStyle(fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 16.0),
-              Container(
-                color: Colors.black,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.local_taxi_rounded,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Kadıköy - Ataşehir',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, color: Colors.white),
-                        ),
-                      ),
-                      Text(
-                        '56,90tl',
-                        style: TextStyle(
-                            color: Colors.green, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
                 ),
+                Container(
+                    width: 50.0,
+                    height: 50.0,
+                    child: PopupMenuButton(
+                        color: ColorHelper.headerPopupMenuBg,
+                        child: IconHelper.headerPopupMenuIcon,
+                        itemBuilder: (context) => [
+                              //popup items create loop
+                              for (var element
+                                  in TextHelper.headerPopupMenuButtonGroup)
+                                PopupMenuItem(
+                                    child: Text(element[0]), value: element[1])
+                            ],
+                        onSelected: (choice) {
+                          switch (choice) {
+                            case 1:
+                              break;
+                            case 2:
+                              break;
+                            case 3:
+                              break;
+                            case 4:
+                              break;
+                            case 5:
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => BodySettings()));
+                          }
+                        })),
+              ],
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(50.0),
+            child: TabBar(tabs: [
+              Tab(
+                text: TextHelper.tabBarView1,
               ),
-              const SizedBox(height: 16.0),
-              Placeholder(
-                strokeWidth: 1,
-                fallbackHeight: 30,
-                fallbackWidth: 100,
-                color: AppColorContants.placeHolderColor,
+              Tab(
+                text: TextHelper.tabBarView2,
               ),
-              GestureDetector(
-                onTap: () {
-                  print('selected tap');
-                },
-                onDoubleTap: () {
-                  print('selected doubletap');
-                },
-                child: Icon(Icons.add_a_photo),
+              Tab(
+                text: TextHelper.tabBarView3,
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => StatelessPage()));
-                  },
-                  child: Text(AppTextContants.buttonNextPage)),
-              Divider(
-                thickness: 20,
-                color: AppColorContants.placeHolderColor,
-              ),
-              FlutterLogo(
-                size: 50.0,
-              ),
-              TextButton(
-                  onPressed: () {
-                    showModalBottomSheet(
-                        context: context, builder: (context) => Column(
-                      children: [
-                        Text('Deneme yarı açılır pencere'),
-                      ],
-                    ));
-                  },
-                  child: Text(AppTextContants.showProgramingLanguage)),
-            ],
+            ]),
           ),
         ),
-      ),*/
+        body: TabBarView(children: <Widget>[
+          BodyChats(),
+          BodyStatus(),
+          BodyCall(),
+        ]),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: firebaseDbAddData,
+          tooltip: 'Increment',
+          child: new Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
